4f1ed6af134df54fce3fde6e3f178a47cf520b97
Hemanth Kumar <90673550+Hemanthoo7@users.noreply.github.com>
2022-08-05 10:48:07 +0530

    Add files via upload

--- Custom/Dropdown/Block/Index.php
+++ Custom/Dropdown/Block/Index.php
@@ -2,21 +2,13 @@
 namespace Custom\Dropdown\Block;
 class Index extends \Magento\Framework\View\Element\Template
 {
-
 protected function _prepareLayout()
 {
-
       return parent ::_prepareLayout();
 }
-
-
-
-
     public function getContentForDisplay()
     {
-
         return '/dropdown/index/index';
-
     }
 }
 
--- Custom/Dropdown/Controller/Index/Index.php
+++ Custom/Dropdown/Controller/Index/Index.php
@@ -4,7 +4,6 @@ namespace Custom\Dropdown\Controller\Index;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
-
 class Index extends Action
 { /**
      * @var Magento\Framework\View\Result\PageFactory
@@ -24,13 +23,10 @@ class Index extends Action
         parent::__construct($context);
 
     }
-
     public function execute()
     {
-
         $resultPage = $this->_resultPageFactory->create();
         $resultPage->getConfig()->getTitle()->prepend(__(' heading '));
-
         $block = $resultPage->getLayout()
                 ->createBlock('Custom\Dropdown\Block\Index')
                 ->setTemplate('Custom_Dropdown::list.phtml')
